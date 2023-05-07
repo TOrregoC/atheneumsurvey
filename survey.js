e4ae2c0b2ddd91fd5714f4d9242852105120da14
TOrregoC <108294433+TOrregoC@users.noreply.github.com>
2023-05-07 11:21:10 -0400

    Add files via upload

---
+++ survey.js
@@ -0,0 +1,34 @@
+function getQueryParams() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return {
+    proj: urlParams.get('proj'),
+    RDID: urlParams.get('RDID'),
+    UID: urlParams.get('UID')
+  };
+}
+
+function displaySurveyMessage() {
+  const { RDID } = getQueryParams();
+  const surveyMessage = document.getElementById('survey-message');
+
+  let message = '';
+
+  switch (RDID) {
+    case '1':
+      message = 'Thank you for taking this survey. Your effort is greatly appreciated!';
+      break;
+    case '2':
+      message = 'Thank you for your willingness to participate. Unfortunately, you do not meet the requirements for this survey, We appreciate the effort involved from your side and we will reach out for future projects which are a better fit for your area of expertise.';
+      break;
+    case '3':
+      message = 'Thank you very much for your time. Unfortunately, we have already received too many responses similar to yours. We appreciate your effort involved from your side and will reach out for future projects which are a fit for your area of expertise.';
+      break;
+    default:
+      message = 'Error: Invalid survey status.';
+  }
+
+  surveyMessage.innerHTML = `<p>${message}</p>`;
+}
+
+window.onload = displaySurveyMessage;
