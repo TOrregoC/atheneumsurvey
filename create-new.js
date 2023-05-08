81526e55cd07775160ef57847a2d4955fa92980b
TOrregoC <108294433+TOrregoC@users.noreply.github.com>
2023-05-08 12:21:27 -0400

    Update create-new.js

--- create-new.js
+++ create-new.js
@@ -1,146 +1,24 @@
-import { addProjectToFirestore } from "./projects.js";
-// Import the necessary Firestore functions
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-} from "https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js";
+import { addProjectToFirestore, db } from "./projects.js";
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js";
 
 const baseURL = "https://example.com/your-survey";
 
-function buildURL(proj, RDID, UID) {
-  return `${baseURL}?proj=${encodeURIComponent(proj)}&RDID=${encodeURIComponent(RDID)}&UID=${encodeURIComponent(UID)}`;
-}
-  
-function populateProjectList() {
-  const projectList = document.getElementById('project-list');
-  if (!projectList) {
-    console.error('project-list element not found');
-    return;
-  }
-  console.log('projectList:', projectList); // Add this line
-  projectList.innerHTML = '';
-
-  // Read projects data from local storage
-  const projectsData = JSON.parse(localStorage.getItem('projects')) || [];
-  console.log('projectsData:', projectsData);
-
-  projectsData.forEach((project, index) => {
-    const listItem = document.createElement('li');
-    listItem.textContent = `${project.apCode} - ${project.name}`;
-    listItem.addEventListener('click', () => openProject(index));
-    projectList.appendChild(listItem);
-  });
-}
-
-
-function openProject(index) {
-    const projectsData = JSON.parse(localStorage.getItem('projects')) || [];
-    const project = projectsData[index];
-    const rightColumn = document.querySelector('.right-column');
-    
-    const rowData = project.data.map(entry => {
-        const status = entry.RDID === 1 ? 'Complete' : (entry.RDID === 2 ? 'Terminate' : 'Overquota');
-        return `<tr>
-                    <td>${entry.UID}</td>
-                    <td>${status}</td>
-                    <td>${entry.Date}</td>
-                </tr>`;
-    }).join('');
-
-    rightColumn.innerHTML = `
-        <h3>Here are your redirects</h3>
-        <p>Complete: ${buildURL(project.proj, 1, '')}UID_VALUE</p>
-        <p>Terminate: ${buildURL(project.proj, 2, '')}UID_VALUE</p>
-        <p>Overquota: ${buildURL(project.proj, 3, '')}UID_VALUE</p>
-
-        
-        <table id="project-details">
-            <thead>
-                <tr>
-                    <th>UID</th>
-                    <th>Status</th>
-                    <th>Date</th>
-                </tr>
-            </thead>
-            <tbody>
-                ${rowData}
-            </tbody>
-        </table>
-        <button id="download-table" style="float: right;">Download Table</button>
-    `;
-
-    const downloadButton = document.getElementById('download-table');
-    downloadButton.addEventListener('click', (event) => {
-        event.preventDefault();
-        downloadTableAsExcel('project-details', `${project.name}-redirects.xlsx`);
-    });
-}
-
-function downloadTableAsExcel(tableId, filename) {
-    const table = document.getElementById(tableId);
-    const ws = XLSX.utils.table_to_sheet(table);
-    const wb = XLSX.utils.book_new();
-
-    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-    XLSX.writeFile(wb, filename, { bookType: 'xlsx', type: 'binary' });
-}
-
-window.onload = () => {
-  console.log('window.onload called');
-  const createProjectForm = document.getElementById('create-project-form');
-  if (createProjectForm) {
-    createProjectForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-      createNewProject();
-    });
-  }
-};
-
-async function fetchProjectData() {
-  const projectsData = [];
-  try {
-    const db = getFirestore();
-    const querySnapshot = await getDocs(collection(db, "responses"));
-    querySnapshot.forEach((doc) => {
-      const responseData = doc.data();
-      const projectIndex = projectsData.findIndex(
-        (project) => project.proj === responseData.proj
-      );
-      if (projectIndex > -1) {
-        projectsData[projectIndex].data.push(responseData);
-      } else {
-        projectsData.push({
-          proj: responseData.proj,
-          data: [responseData],
-        });
-      }
-    });
-    return projectsData;
-  } catch (error) {
-    console.error("Error fetching project data: ", error);
-    return [];
-  }
-}
-
 async function createNewProject() {
-  const apCode = document.getElementById('ap-code').value;
-  const projectName = document.getElementById('project-name').value;
+  const apCode = document.getElementById("ap-code").value;
+  const projectName = document.getElementById("project-name").value;
   const proj = generateRandomID();
 
   const newProject = {
     apCode,
     name: projectName,
     proj,
-    data: []
+    data: [],
   };
 
   try {
     // Save the new project to Firestore
-    const firestore = getFirestore();
-    await addProjectToFirestore(firestore, newProject);
-    alert('Project created successfully.');
+    await addProjectToFirestore(db, newProject);
+    alert("Project created successfully.");
     window.location.href = "projects.html";
   } catch (error) {
     console.error("Error adding project to Firestore:", error);
@@ -148,7 +26,17 @@ async function createNewProject() {
   }
 }
 
-
 function generateRandomID() {
   return Math.random().toString(36).substr(2, 10);
 }
+
+window.onload = () => {
+  console.log("window.onload called");
+  const createProjectForm = document.getElementById("create-project-form");
+  if (createProjectForm) {
+    createProjectForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      createNewProject();
+    });
+  }
+};
